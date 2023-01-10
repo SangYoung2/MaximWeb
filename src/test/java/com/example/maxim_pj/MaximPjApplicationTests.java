bbf054b7012287f347cfacc02e0d7b3e1e4a2910
KSY <a123@naver.com>
2023-01-10 14:34:53 +0900

    23.01.10

--- src/test/java/com/example/maxim_pj/MaximPjApplicationTests.java
+++ src/test/java/com/example/maxim_pj/MaximPjApplicationTests.java
@@ -1,13 +1,12 @@
-package com.example.maxim_pj;
-
-import com.example.maxim_pj.service.MainService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MaximPjApplicationTests {
-    @Autowired
-    MainService mainService;
-
-}
+//package com.example.maxim_pj;
+//
+//import com.example.maxim_pj.service.MainService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MaximPjApplicationTests {
+//    @Autowired
+//    MainService mainService;
+//
+//}
